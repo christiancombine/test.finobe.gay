2ed00a8f0754cf48bebf1bccdc65d406fca7892d
christiancombine <piotrkafarski04@gmail.com>
2026-05-18 21:11:50 +0200

    easy site

---
+++ index.php
@@ -0,0 +1,3 @@
+<p>Woke16 Dashboard<p>
+<h>be happy<h>
+<p>uhh PAGE UNFINISHED<p>
---
+++ public/index.php
@@ -0,0 +1,4 @@
+<p>finobe.gay/woke16 is under fixing<p>
+    <p>dm christian combine if u have any questions<p>
+        <p> should be working soon... <p>
+            <p>while woke16 is down play HEXAGON : discord.gg/finobe<p>
